2f1e35464a2f2d21cff0ca468c001e8390b5e47f
abhishekvlog <abhishek.kumar.kps25@gmail.com>
2021-09-27 16:32:52 +0530

    commit service class

---
+++ app/src/main/java/com/example/music_player_sprint/MusicService.kt
@@ -0,0 +1,86 @@
+package com.example.music_player_sprint
+
+import android.app.Notification
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+import android.app.NotificationManager
+
+import android.app.NotificationChannel
+
+import android.os.Build
+
+import androidx.core.app.NotificationCompat
+import java.util.*
+
+
+class MusicService : Service() {
+    var mediaPlayer : MediaPlayer?= null
+    var isPlaying : Boolean = false
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+    public fun play(){
+        if(mediaPlayer != null){
+            isPlaying =true
+            mediaPlayer!!.start()
+            showNotificationAndStartForeGround()
+        }
+
+    }
+    public fun pause(){
+        if(mediaPlayer != null){
+            isPlaying =false
+            mediaPlayer!!.stop()
+            showNotificationAndStartForeGround()
+        }
+
+    }
+    public fun delete(){
+        if (mediaPlayer != null){
+            isPlaying = false
+        }
+    }
+
+    public class ServiceBinder : Binder() {
+        fun getMusivService MusicService(){
+            return MusicService
+        }
+    }
+
+
+    private fun showNotificationAndStartForeGround() {
+        createChannel()
+        var notificationBuilder: NotificationCompat.Builder? = null
+        notificationBuilder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setContentTitle("Service is running : yaay")
+            .setContentText("name is android")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        var notification: Notification? = null
+        notification = notificationBuilder.build()
+        startForeground(1, notification)
+    }
+
+
+    /*
+Create noticiation channel if OS version is greater than or eqaul to Oreo
+*/
+    fun createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel("CHANNEL_ID", "Nimit", NotificationManager.IMPORTANCE_HIGH)
+            channel.description = "Notifications"
+            Objects.requireNonNull(this.getSystemService(NotificationManager::class.java))
+                .createNotificationChannel(channel)
+        }
+    }
+}
--- app/src/main/java/com/example/music_player_sprint/SongViewHolder.kt
+++ app/src/main/java/com/example/music_player_sprint/SongViewHolder.kt
@@ -2,13 +2,12 @@ package com.example.music_player_sprint
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_layout.view.*
 
 class SongViewHolder(private val view : View) : RecyclerView.ViewHolder(view){
     fun setData(resultsDTO: ResultsDTO){
-        if (resultsDTO.artistViewUrl != null){
-
-        }
+        Glide.with(view.tvImagePoster).load(resultsDTO.trackViewUrl).placeholder(R.drawable.shapeofyou).into(view.tvImagePoster)
         view.tvNameOfSong.text = resultsDTO.trackName
         view.tvartistOfSong.text = resultsDTO.artistName;
     }
